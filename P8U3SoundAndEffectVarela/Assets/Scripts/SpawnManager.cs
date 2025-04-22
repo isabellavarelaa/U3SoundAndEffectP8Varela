2ee65402064f72820ab4bdf2b1141ee261bbff66
isabellavarelaa <727144@student.dusd.net>
2025-04-22 15:26:25 -0700

    edit
    edit

--- P8U3SoundAndEffectVarela/Assets/Scripts/SpawnManager.cs
+++ P8U3SoundAndEffectVarela/Assets/Scripts/SpawnManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
-    private Vector3 spawnPos = new Vector3 (25, 0, 0);
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +15,11 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void SpawnObstacle()
+    {
+
     }
 }
