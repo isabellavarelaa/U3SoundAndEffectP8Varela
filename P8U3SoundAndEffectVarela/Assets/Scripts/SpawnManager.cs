b3c1bfd531771096e492636f0af5dd3ef4348a3f
isabellavarelaa <727144@student.dusd.net>
2025-04-24 15:28:43 -0700

    unit 3
    unit 3

--- P8U3SoundAndEffectVarela/Assets/Scripts/SpawnManager.cs
+++ P8U3SoundAndEffectVarela/Assets/Scripts/SpawnManager.cs
@@ -6,10 +6,15 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
+    private PlayerController playerControllerScript;
+
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
     }
 
     // Update is called once per frame
@@ -20,6 +25,9 @@ public class SpawnManager : MonoBehaviour
 
     void SpawnObstacle()
     {
-
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
     }
 }
